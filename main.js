9e61f42609efd30d9a611027d8253207708831db
Micheli <mat.micheli99@gmail.com>
2020-04-23 15:20:01 +0200

    animation css

---
+++ main.js
@@ -0,0 +1,9 @@
+function divShow() {
+    var premiere = document.getElementById('premiere');
+    var deuxieme = document.getElementById('deuxieme');
+    premiere.style.display = "none";
+    premiere.style.visibility = "hidden";
+    deuxieme.style.display = "block";
+    deuxieme.style.visibility = "visible";
+}
+
