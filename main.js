51291ea3975e87dc0b45ae6d0d33e77808a4eff9
Micheli <mat.micheli99@gmail.com>
2020-04-24 10:01:37 +0200

    test pour githubpage

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function divShow() {
-    var premiere = document.getElementById('premiere');
-    var deuxieme = document.getElementById('deuxieme');
+    let premiere = document.getElementById('premiere');
+    let deuxieme = document.getElementById('deuxieme');
     premiere.style.display = "none";
     premiere.style.visibility = "hidden";
     deuxieme.style.display = "block";
